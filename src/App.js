50b6e779f1e058ec2ea452b566690a33d08487d9
Cesar Muro <cesaramuroc@gmail.com>
2022-05-20 13:18:18 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from '@mui/material';
+
+function App() {
+  return (
+    <div className="App">
+
+<Button variant='outlined' color='primary'>Hello world </Button> 
+ 
+     </div>
+  );
+}
+
+export default App;
