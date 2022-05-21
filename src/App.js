08d156222a9b92d43afcdb609251863b66f6c75d
cesar muro <cesaramuroc@gmail.com>
2022-05-20 21:02:25 -0400

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
 
-<Button variant='outlined' color='primary'>Oscar es pato</Button> 
+<Button variant='outlined' color='primary'>Oscar es pato Update</Button> 
  
      </div>
   );
