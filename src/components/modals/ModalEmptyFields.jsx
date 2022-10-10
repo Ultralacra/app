28c248af43a4bc9b3084e936e569753a06dfb0be
Victor Velasco <velascorojasvictorjavier@gmail.com>
2022-10-09 21:32:44 -0400

    Modal

--- src/components/modals/ModalEmptyFields.jsx
+++ src/components/modals/ModalEmptyFields.jsx
@@ -1,27 +1,23 @@
 
 import React, { useState, useEffect } from "react";
-import swal from 'sweetalert2'
-
-
-// eslint-disable-next-line react-hooks/rules-of-hooks
+import swal from 'sweetalert2';
 
 function ModalEmptyFields() {
 
-const EmptyField = () => {
-    console.log('ffffffffff')
-    swal.fire({
-        
-        toast: true,
-        position: 'top-end',
-        showConfirmButton: false,
-        timer: 1500,
-        timerProgressBar: true,
-        icon: 'error',
-        title: 'Debe completar todos los campos',
+    const EmptyField = () => {
+        swal.fire({
+            
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 1500,
+            timerProgressBar: true,
+            icon: 'error',
+            title: 'Debe completar todos los campos',
 
-    })
-}
+        })
+    }
+    
     return (<div>{EmptyField()}</div>)
-
 }
 export default ModalEmptyFields;
