a427e81f520df6855b17724bc07d43bbee252e07
cesar muro <cesaramuroc@gmail.com>
2022-11-01 18:32:21 -0300

    update

--- src/components/Appbar/Header/AppBar.jsx
+++ src/components/Appbar/Header/AppBar.jsx
@@ -1,22 +1,28 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import './AppBar.css';
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+import "./AppBar.css";
 
 const drawerWidth = 240;
-const navItems = ['Nosotros', '¿Cómo funciona?', 'Preguntas Frecuentes', "Contacto"  ];
+const navItems = [
+  "Nosotros",
+  "¿Cómo funciona?",
+  "Preguntas Frecuentes",
+  "Contacto",
+];
 
 function DrawerAppBar(props) {
   const { window } = props;
@@ -27,70 +33,99 @@ function DrawerAppBar(props) {
   };
 
   const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
       <Typography variant="h6" sx={{ my: 2 }}>
-      VALINKPAY
+        VALINKPAY
       </Typography>
       <Divider />
       <List>
         {navItems.map((item) => (
           <ListItem key={item}>
-            <ListItemButton sx={{ textAlign: 'center' }}>
+            <ListItemButton sx={{ textAlign: "left" }}>
               <ListItemText primary={item} />
             </ListItemButton>
           </ListItem>
         ))}
       </List>
+      <ListItem>
+            <ListItemButton>
+              <ListItemText>
+                <Button>
+                  Iniciar Sesión
+                </Button>
+              </ListItemText>
+          
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton>
+              <ListItemText>
+                <Button variant="contained">
+                 Acceso Clientes
+                </Button>
+              </ListItemText>
+            </ListItemButton>
+          </ListItem>
     </Box>
+    
   );
 
-  const container = window !== undefined ? () => window().document.body : undefined;
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box className='test'>
-      <AppBar component="nav"
-      sx={{
-        flexGrow: 1,
-        bgcolor: "#eceff1",
-        display: 'flex',
-        justifyContent: 'space-between',
-        overflow: 'hidden',
-      }}>
+    <Box className="test">
+      <AppBar
+        component="nav"
+        sx={{
+          flexGrow: 1,
+          bgcolor: "#eceff1",
+          display: "flex",
+          justifyContent: "space-between",
+          overflow: "hidden",
+        }}
+      >
         <Toolbar>
           <IconButton
-
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
+            sx={{ mr: 2, display: { sm: "none" } }}
           >
             <MenuIcon />
           </IconButton>
           <img
-          src="http://valinkgroup.com/wp-content/uploads/2022/05/Negro.png"
-          alt="logo"
-          width="150"
+            src="http://valinkgroup.com/wp-content/uploads/2022/05/Negro.png"
+            alt="logo"
+            width="150"
           />
-          <Box sx={{ display: { xs: 'none', sm: 'block' } }}
-          className='list-items-nav'>
+          <Box
+            sx={{ display: { xs: "none", sm: "block" } }}
+            className="list-items-nav"
+          >
             {navItems.map((item) => (
-              <Button key={item} sx={{
-                color: 'black',
-                fontSize: '0.9rem',
-                fontWeight: '100',
-                fontFamily: "roboto",
-                textTransform: 'none',
-                margin: '0 1rem',
-                '&:hover': {
-                  bgcolor: '#006D8E',
-                  color: 'white',
-                },
-  
-              }}>
+              <Button
+                key={item}
+                sx={{
+                  color: "black",
+                  fontSize: "0.9rem",
+                  fontWeight: "100",
+                  fontFamily: "roboto",
+                  textTransform: "none",
+                  margin: "0 1rem",
+                  "&:hover": {
+                    bgcolor: "#006D8E",
+                    color: "white",
+                  },
+                }}
+              >
                 {item}
               </Button>
             ))}
+
           </Box>
+      
+
         </Toolbar>
       </AppBar>
       <Box component="nav">
@@ -103,8 +138,11 @@ function DrawerAppBar(props) {
             keepMounted: true, // Better open performance on mobile.
           }}
           sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
         >
           {drawer}
@@ -112,7 +150,6 @@ function DrawerAppBar(props) {
       </Box>
       <Box component="main" sx={{ p: 3 }}>
         <Toolbar />
-        
       </Box>
     </Box>
   );
