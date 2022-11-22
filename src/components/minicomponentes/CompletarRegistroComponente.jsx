29f320079213d0fa012ee2e8a19da86904176820
Victor-Velasco <velascorojasvictorjavier@gmail.com>
2022-11-22 11:13:53 -0400

    Cambio TextField

--- src/components/minicomponentes/CompletarRegistroComponente.jsx
+++ src/components/minicomponentes/CompletarRegistroComponente.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Box from "@mui/material/Box";
 import { Typography } from "@mui/material";
 import Grid from "@mui/material/Grid";
