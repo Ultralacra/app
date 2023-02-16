bd179972eff5a7f3dc35ad41dab7ae1254e6fe54
Cesar <cesaramuroc@gmail.com>
2023-02-16 10:23:23 -0300

    update

--- src/components/Appbar/Header/AppBar.jsx
+++ src/components/Appbar/Header/AppBar.jsx
@@ -26,7 +26,7 @@ function CollapsibleExample() {
           </Nav>
           <Nav className='nav-botones' >
             <Link className='btn' to="/register-page">Registro</Link>
-            <Link  className='btn btn-2'  to="/login">Acceso Clientes</Link>
+            <Link  className='btn btn-2'  to="/login">Acceso Clientessss</Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
