a97d26f832739b2e4e073f74beb05c6d7dbd2ff2
Cesar <cesaramuroc@gmail.com>
2023-02-16 12:44:45 -0300

    update

--- src/components/Appbar/Header/AppBar.jsx
+++ src/components/Appbar/Header/AppBar.jsx
@@ -20,7 +20,7 @@ function CollapsibleExample() {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#features">Inicio</Nav.Link>
-            <Nav.Link id="#que-es-valinkpay" >¿Qué es ValinkPay?</Nav.Link>
+            <Nav.Link id="#que-es-valinkpay" >¿Qué es ValinkPay?.</Nav.Link>
             <Nav.Link id="#que-es-valinkpay" >Beneficios</Nav.Link>
             <Nav.Link id="#que-es-valinkpay" >Contacto</Nav.Link>
           </Nav>
